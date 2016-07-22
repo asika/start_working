4e43b1850c7b488a2af45847f3c83943ad47bad4
Ching-Hsiang Kuo <chk@bigobject.io>
2016-07-22 11:31:25 +0800

    add dev essentials

--- start.sh
+++ start.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 set -ex
 
+apt-get update
+
 # install dev packages
+apt-get install -y build-essential
+apt-get install -y python-dev
+
+# dev tools
+apt-get install -y exuberant-ctags
+
+# configure vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp vim/.vimrc vim/.my_vimrc $HOME
+
+vim -c VundleUpdate -c quitall
 
 # install python deps
 pip install ipython
