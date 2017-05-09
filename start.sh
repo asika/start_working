8221964c38a2239b38c8d2ca59f72872eb387b5e
asika Kuo <asika.tw@gmail.com>
2017-05-09 10:19:59 +0800

    add dev essentials

--- start.sh
+++ start.sh
@@ -5,13 +5,16 @@ apt-get update
 
 # install esssential tools
 apt-get install -y screen
+apt-get install -y vim 
 
 # install dev packages
 apt-get install -y build-essential
 apt-get install -y python-dev
+apt-get install -y default-jre
 
 # dev tools
 apt-get install -y exuberant-ctags
+apt-get install -y htop 
 
 # configure vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim | true
@@ -20,4 +23,7 @@ cp vim/.vimrc vim/.my_vimrc $HOME
 vim -c VundleUpdate -c quitall
 
 # install python deps
+pip install --upgrade pip
 pip install -r pip/requirements.txt
+
+cp .screenrc $HOME
